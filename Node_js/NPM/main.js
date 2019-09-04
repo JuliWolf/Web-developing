b0f148d53c94a19e696e4f6478d0c5ac0ca7e5ff
JuliWolf <for.julia96@gmail.com>
2019-09-04 10:37:11 +0300

    NPM settings

---
+++ Node_js/NPM/main.js
@@ -0,0 +1,46 @@
+// Install packages.json from current directory
+// npm install
+
+//Install specific package
+// npm install <package-name>
+
+
+// Flags
+//install and addss the entry to the package.json file dependencies
+// --save
+
+//install and adds the entry to the package.json file devDependencies
+// --save-dev
+
+
+//Update
+// npm update
+
+//Update specific package
+// npm update <package-name>
+
+
+//Running Tasks
+// npm run <task-name>
+// {
+//     "scripts": {
+//         "watch": "webpack --watch --progress --colors --config webpack.conf.js",
+//         "dev": "webpack --progress --colors --config webpack.conf.js",
+//         "prod": "NODE_ENV=production webpack -p --config webpack.conf.js",
+//     },
+// }
+
+// npm run watch
+// npm run dev
+// npm run prod
+
+
+/*WHERE NPM INSTALL PACKAGES*/
+//locally
+// npm install <package-name>
+
+//globally
+// npm install -g <package-name>
+
+//Command to know version and path
+// nvm
