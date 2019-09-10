6691d0a3e139f5298834d8d71bc7412c1a1b94fa
JuliWolf <for.julia96@gmail.com>
2019-09-10 12:34:10 +0300

    Install packages for login and sign up installed passport js passport-local validator bcrypt express-session connect-flash

--- Node_js/5. Express/myapp/routes/index.js
+++ Node_js/5. Express/myapp/routes/index.js
@@ -5,6 +5,8 @@ let landing = require('../controllers/landing');
 let user = require('../controllers/user');
 router.get('/login', user.show_login);
 router.get('/signup', user.show_signup);
+router.post('/login', user.login);
+router.post('/signup', user.signup);
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
