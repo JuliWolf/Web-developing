88ea66beb0f72336d5111c66fa6326826d589962
JuliWolf <for.julia96@gmail.com>
2019-09-13 12:45:51 +0300

    Components props

--- JAVASCRIPT/9. React.js/2. React-app/src/app/components/App.js
+++ JAVASCRIPT/9. React.js/2. React-app/src/app/components/App.js
@@ -3,6 +3,10 @@ import { Header } from "./Header";
 import { Home } from "./Home";
 
 function App() {
+    var user= {
+        name: 'Anna',
+        hobbies: ["Sports", "Reading"]
+    };
   return (
     <div className="container">
         <div className="row">
@@ -12,7 +16,9 @@ function App() {
         </div>
         <div className="row">
             <div className="col-xs-10 col-sx-offset-1">
-                <Home/>
+                <Home name={"Julia"} age={23} user={user}>
+                    <p>This is a paragraph!</p>
+                </Home>
             </div>
         </div>
     </div>
--- JAVASCRIPT/9. React.js/2. React-app/src/app/components/Home.js
+++ JAVASCRIPT/9. React.js/2. React-app/src/app/components/Home.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class Home extends React.Component{
     render(){
+        console.log(this.props);
+        let content = "";
+        if(true){
+            content = <p>Hello!</p>;
+        }
+        let text = "Something";
         return(
             <div>
                 <p>In a new Component!</p>
+                { content }
+                <p> { text }</p>
+                <p> Your name is { this.props.name }, your age is {this.props.age} </p>
+                <p>User Object => {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, index) => <li key={index}>{hobby}</li> )}
+                    </ul>
+                </div>
+                <hr/>
+                {this.props.children}
             </div>
         )
     }
 }
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  user: PropTypes.object,
+    children: PropTypes.element.isRequired
+};
