b77b744359bc4bf44bfa6d20d9e748f6de3e2da6
JuliWolf <for.julia96@gmail.com>
2020-03-11 18:09:29 +0300

    Lesson 6 added component for selection location on map

--- JAVASCRIPT/9. React.js/Letters Social/src/components/map/LocationTypeAhead.js
+++ JAVASCRIPT/9. React.js/Letters Social/src/components/map/LocationTypeAhead.js
@@ -16,6 +16,59 @@ export default class LocationTypeAhead extends Component {
             selectedLocation: null
         };
         this.mapbox = new MapBox(process.env.MAPBOX_API_TOKEN);
+        this.handleLocationUpdate = this.handleLocationUpdate.bind(this);
+        this.handleSearchChange = this.handleSearchChange.bind(this);
+        this.handleSelectLocation = this.handleSelectLocation.bind(this);
+        this.resetSearch = this.resetSearch.bind(this);
+    }
+
+    componentWillUnmount(){
+        this.resetSearch()
+    }
+
+    handleLocationUpdate(location){
+        this.setState(() => {
+            return {
+                text: location.name,
+                locations: [],
+                selectedLocation: location
+            };
+        });
+        this.props.onLocationUpdate(location);
+    }
+
+    handleSearchChange(e){
+        const text = e.target.value;
+        this.setState(() => ({ text }));
+        if(!text) return;
+        this.mapbox.geocodeForward(text, {}).then(loc => {
+            if(!loc.entiry.features || !loc.entity.features.length){
+                return;
+            }
+            const location = loc.entity.features.map(feature => {
+                const [lng, lat] = feature.center;
+                return {
+                    name: feature.place_name,
+                    lat,
+                    lng
+                };
+            });
+            this.setState(() => ({ locations }));
+        })
+    }
+
+    resetSearch() {
+        this.setState(() => {
+            return {
+                text: '',
+                locations: [],
+                selectedLocation: null
+            };
+        });
+    }
+
+    handleSelectLocation() {
+        this.props.onLocationSelect(this.state.selectedLocation);
     }
 
     render () {
